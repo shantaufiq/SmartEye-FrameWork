57c807fe516f7e01635f23323271da9253b81138
shantaufiq <shantaufiq021@gmail.com>
2023-11-07 13:16:00 +0700

    add: virtual-keyboard-with-Microsoft-Toolkit

---
+++ Assets/_Sandboxing/Scripts/Keyboard/KeyboardController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
+public class KeyboardController : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    public float distance = 0.5f;
+    public float verticalOffset = -0.5f;
+
+    public Transform possitionCam;
+
+    void Start()
+    {
+        inputField.onSelect.AddListener(x => OpenKeyboard());
+    }
+
+    public void OpenKeyboard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+
+        Vector3 direction = possitionCam.forward;
+        direction.y = 0;
+        direction.Normalize();
+
+        Vector3 targetPos = possitionCam.position + direction * distance + Vector3.up * verticalOffset;
+        NonNativeKeyboard.Instance.RepositionKeyboard(targetPos);
+
+        SetCaretColorAlpha(1);
+
+        NonNativeKeyboard.Instance.OnClosed += Instance_OnClosed;
+    }
+
+    private void Instance_OnClosed(object sender, System.EventArgs a)
+    {
+        SetCaretColorAlpha(0);
+        NonNativeKeyboard.Instance.OnClosed -= Instance_OnClosed;
+    }
+
+    public void SetCaretColorAlpha(float value)
+    {
+        inputField.customCaretColor = true;
+        Color caretColor = inputField.caretColor;
+        caretColor.a = value;
+        inputField.caretColor = caretColor;
+    }
+}
